ce34058f7fcb0c446f50049ca11186df323a5589
hernan thiers <hernanthiers@gmail.com>
2012-09-04 22:38:43 -0300

    config generales para repo

--- config.php
+++ config.php
@@ -1,19 +1,16 @@
 <?php
 $config = Config::singleton();
 
-$config->set('apachePath', 'E:/xampp/');
-$config->set('rootPath', '/tiempo_trabajo/');
+$config->set('apachePath', '');
+$config->set('rootPath', '/CAService/');
 $config->set('controllersFolder', 'controllers/');
 $config->set('modelsFolder', 'models/');
 $config->set('viewsFolder', 'views/');
 
 $config->set('dbhost', 'localhost');
-#$config->set('dbname', 'suyay_registrotrabajo');
-#$config->set('dbuser', 'suyay_regtrabajo');
-#$config->set('dbpass', 'I2_;(A@{TNy+');
 $config->set('dbname', 'tiempo_trabajo');
-$config->set('dbuser', 'root');
-$config->set('dbpass', 'walkirias');
+$config->set('dbuser', '');
+$config->set('dbpass', '');
 
 $config->set('timezone', 'America/Santiago');
 $config->set('debug', false);
