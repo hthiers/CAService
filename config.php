4c203aaf54b3f6b62aebf261a6da37abd4ed28c1
hernan thiers <hernan@linux-dzuk.site>
2012-10-02 20:53:50 -0300

    cambios menores.

--- config.php
+++ config.php
@@ -1,20 +1,16 @@
 <?php
 $config = Config::singleton();
 
-$config->set('apachePath', 'E:/xampp/');
-#$config->set('rootPath', '/'); // para suyay.cl
-$config->set('rootPath', '/CAService/');
+$config->set('apachePath', '/home/hernan/public_html/');
+$config->set('rootPath', '/~hernan/CAService/');
 $config->set('controllersFolder', 'controllers/');
 $config->set('modelsFolder', 'models/');
 $config->set('viewsFolder', 'views/');
 
 $config->set('dbhost', 'localhost');
-#$config->set('dbname', 'suyay_registrotrabajo'); // para suyay.cl
-#$config->set('dbuser', 'suyay_regtrabajo'); // para suyay.cl
-#$config->set('dbpass', 'I2_;(A@{TNy+'); // para suyay.cl
 $config->set('dbname', 'cas_mt_db');
 $config->set('dbuser', 'root');
-$config->set('dbpass', 'walkirias');
+$config->set('dbpass', 'walkirias84');
 
 $config->set('timezone', 'America/Santiago');
 $config->set('debug', true);
