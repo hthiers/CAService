8f937c9ab09cb6717e8e2f1c76baf3fa0aa1651d
Hernan Thiers LG <practica.alumnoit@lge.com>
2012-10-24 11:15:56 -0300

    Ajuste en funcion secondsToTime (utilidades) para formato de tiempo total.

--- views/lib/utils.js
+++ views/lib/utils.js
@@ -16,6 +16,13 @@ function secondsToTime(secs)
     var divisor_for_seconds = divisor_for_minutes % 60;
     var seconds = Math.ceil(divisor_for_seconds);
 
+    if(hours<10)
+        hours = '0'+hours;
+    if(minutes<10)
+        minutes = '0'+minutes;
+    if(seconds<10)
+        seconds = '0'+seconds;
+
     var obj = {
         "h": hours,
         "m": minutes,
