0b75b9909b783b6f34c39403a4ef8b4bd41ff8b8
Hernan Thiers LG <practica.alumnoit@lge.com>
2012-12-19 16:47:36 -0300

    menores...

--- views/templates/header.tpl.php
+++ views/templates/header.tpl.php
@@ -19,7 +19,7 @@ $navegador = substr($navegador,25,8);
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
 <title>Control tiempos de trabajo - demo no funcional v0.1</title>
 <!-- @import "views/css/custom-theme/jquery-ui-1.8.20.custom.css"; -->
 <style type="text/css">
